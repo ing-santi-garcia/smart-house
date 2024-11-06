7a720e9325f8dd038321735103a90f4f76e7a305
ing-santi-garcia <ing.santiago.garcia10@gmail.com>
2024-11-06 17:51:22 -0500

    intento 3 deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site:'https://ing-santi-garcia.github.io',
-  base:'smart-house'
+  base:'smart-house',
+  output:"static",
 });
